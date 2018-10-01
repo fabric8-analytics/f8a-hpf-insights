750d92d49739b37db531f08b0466bbc18c8c1ae0
Avishkar Gupta <avgupta@redhat.com>
2018-10-01 12:10:20 +0530

    Manually create dir under /tmp so creation does not fail
    Signed-off-by: Avishkar Gupta <avgupta@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 zip -r /tmp/training.zip /src
+mkdir -p /tmp/hpf/
 
 gunicorn --pythonpath /src -b 0.0.0.0:"$SERVICE_PORT" --workers=1 -k sync -t "$SERVICE_TIMEOUT" flask_endpoint:app
