0eec219951299a49fa675b86688e7c021f58e313
dgpatelgit <dhpatel@redhat.com>
2021-05-04 15:59:48 +0530

    fix: [APPAI-1856] Cleanup deployment templates and correct entry script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-zip -r /tmp/training.zip /src
 mkdir -p /tmp/hpf/
 
-gunicorn --pythonpath /src -b 0.0.0.0:6006 --workers=1 -k sync -t 900 flask_endpoint:app
+gunicorn --pythonpath /opt/app-root/src/src -b 0.0.0.0:6006 --workers=1 -k sync -t 900 flask_endpoint:app --log-level $FLASK_LOGGING_LEVEL
