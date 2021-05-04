2475109c14420b9478111677c8f9c266bebb9f4e
dgpatelgit <dhpatel@redhat.com>
2021-05-04 12:12:13 +0530

    chore: [APPAI-1856] Cleanup and migration to app-interface (#170)
    * chore: [APPAI-1856] Cleanup and migration to app-interface
    
    * Added config file

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,24 @@
-FROM registry.centos.org/centos/centos:7
+FROM registry.access.redhat.com/ubi8/python-36:latest
 
-LABEL maintainer="Lucky Suman <lsuman@redhat.com>"
+LABEL name="f8analytics hpf maven insights service" \
+      description="Fabric8 analytic maven insights service for recommendation." \
+      email-ids="dhpatel@redhat.com" \
+      git-url="https://github.com/fabric8-analytics/f8a-hpf-insights" \
+      git-path="/" \
+      target-file="Dockerfile" \
+      app-license="GPL-3.0"
 
-RUN yum install -y epel-release &&\
-    yum install -y zip gcc-c++ git python36-pip python36-requests httpd httpd-devel python36-devel &&\
-    yum clean all
+ENV LANG=en_US.UTF-8 PYTHONDONTWRITEBYTECODE=1
 
-COPY ./requirements.txt /requirements.txt
+COPY ./requirements.txt /opt/app-root
 
-RUN pip3 install --upgrade pip
-RUN pip3 install git+https://github.com/fabric8-analytics/fabric8-analytics-rudra#egg=rudra
-RUN pip3 install -r /requirements.txt && rm /requirements.txt
+RUN pip3 install --no-cache-dir --upgrade pip
+RUN pip3 install --no-cache-dir -r /opt/app-root/requirements.txt
+RUN pip3 install --no-cache-dir Cython==0.29.1 && pip3 install --no-cache-dir hpfrec==0.2.2.9 && pip3 install --no-cache-dir connexion==2.7.0
 
-COPY ./src /src
-COPY ./tools /tools
-COPY ./tests/test_data /tests/test_data
-COPY ./src/config.py.template /src/config.py
+COPY ./src /opt/app-root/src/src
 
 ADD ./entrypoint.sh /bin/entrypoint.sh
-RUN chmod +x /bin/entrypoint.sh
-RUN pip3 install Cython==0.29.1 && pip3 install hpfrec==0.2.2.9
-EXPOSE 6006
 
-ENTRYPOINT ["/bin/entrypoint.sh"]
+EXPOSE 6006
+ENTRYPOINT ["bash", "/bin/entrypoint.sh"]
--- tests/unit_tests/test_flask_endpoint.py
+++ tests/unit_tests/test_flask_endpoint.py
@@ -1,45 +1,98 @@
 """Test the flask endpoints with local data."""
 
-import unittest
 import json
+import mock
+import pytest
+import random
+from faker import Faker
 
-import src.flask_endpoint as rest_api
+faker = Faker()
 
 
-class TestFlaskMethods(unittest.TestCase):
+class MockHPFScoring(mock.Mock):
+    """Mock HPFScoring Class."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the object."""
+        super().__init__(*args, **kwargs)
+
+    def predict(self, input_stack):
+        """Predict the stacks."""
+        pkg_id_list, missing_pkg = self._map_input_to_package_ids(input_stack)
+        if len(pkg_id_list) < len(missing_pkg):
+            return [], list(missing_pkg)
+
+        companion_pkg = list()
+        for pkg in faker.words(len(pkg_id_list)):
+            companion_pkg.append({
+                'package_name': pkg,
+                'cooccurrence_probability': random.randint(30, 99),
+                'topic_list': faker.words(random.randint(0, 3))
+            })
+        return companion_pkg, list(missing_pkg)
+
+    def _map_input_to_package_ids(self, input_stack):
+        """Filter out identified and missing packages."""
+        kwown_packages = {'django', 'flask', 'werkzeug', 'six'}
+        id_pkg = input_stack.intersection(kwown_packages)
+        return id_pkg, input_stack.difference(id_pkg)
+
+
+class MockAmazonS3(mock.Mock):
+    """Mock AWS S3 class."""
+
+    def connect(self):
+        """Mock connect method."""
+        pass
+
+
+@pytest.fixture(scope='module')
+@mock.patch('src.scoring.hpf_scoring.HPFScoring', new_callable=MockHPFScoring)
+@mock.patch('rudra.data_store.aws.AmazonS3', new_callable=MockAmazonS3)
+def api_client(_request, _awss3):
+    """Create an api client instance."""
+    from src.flask_endpoint import app
+    client = app.app.test_client()
+    return client
+
+
+class TestFlaskMethods:
     """Test the flask endpoints with local data."""
 
-    def setUp(self):
-        """Create a api client."""
-        rest_api.app.app.testing = True
-        self.client = rest_api.app.app.test_client()
-        assert self.client is not None
-
-    def test_health_checks(self):
-        """Test the liveness and readiness probes."""
-        self.assertEqual(self.client.get('/api/v1/liveness').status, '200 OK')
-        self.assertEqual(self.client.get(
-            '/api/v1/readiness').status, '200 OK')
-
-    def test_root_path(self):
+    def test_root_path(self, api_client):
         """Test the root path."""
-        self.assertEqual(self.client.get('/').status, '200 OK')
+        resp = api_client.get('/')
+        assert resp is not None
+        assert resp.status_code == 200
 
-    def test_model_details(self):
+    def test_liveness(self, api_client):
+        """Test liveness endpoint."""
+        resp = api_client.get('/api/v1/liveness')
+        assert resp is not None
+        assert resp.status_code == 200
+        assert json.loads(resp.data.decode('UTF-8')) == {"status": "alive"}
+
+    def test_readiness(self, api_client):
+        """Test Readiness endpoint."""
+        resp = api_client.get('/api/v1/readiness')
+        assert resp is not None
+        assert resp.status_code == 200
+        assert json.loads(resp.data.decode('UTF-8')) == {"status": "ready"}
+
+    def test_model_details(self, api_client):
         """Test model details endpoint."""
-        self.assertEqual(self.client.get(
-            '/api/v1/model_details').status, '200 OK')
+        resp = api_client.get('/api/v1/model_details')
+        assert resp is not None
+        assert resp.status_code == 200
 
-    def test_companion_recommendations(self):
+    def test_companion_recommendations(self, api_client):
         """Test companion recommendations endpoint."""
         data = [{"ecosystem": "maven",
                  "package_list": ["org.springframework.cloud:spring-cloud-spring-service-connector"]
                  }]
-        self.assertEqual(self.client.post(
-            '/api/v1/companion_recommendation',
-            data=json.dumps(data),
-            headers={'content-type': 'application/json'}).status, '200 OK')
 
-
-if __name__ == '__main__':
-    unittest.main()
+        resp = api_client.post('/api/v1/companion_recommendation',
+                               data=json.dumps(data),
+                               headers={'content-type': 'application/json'})
+        assert resp is not None
+        assert resp.status_code == 200
